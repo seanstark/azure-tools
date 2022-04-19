a3abedadc54f6848caf6a76bdf450574e5984b22
seanstark-ms <84108246+seanstark@users.noreply.github.com>
2022-04-19 12:25:25 -0700

    Added consolidated IP Address object

--- azure storage/get-azstorageip4address.ps1
+++ azure storage/get-azstorageip4address.ps1
@@ -23,13 +23,16 @@
 
      Examples
      $storageAccountReport = .\get-azstorageip4address.ps1
+
+     Search for an IP Address
+     $ip = '20.150.71.134'
+     $storageAccountReport | Where-Object {$ip -in $_.PrimaryIP4Addresses -or $ip -in $_.SecondaryIP4Addresses}
      
      Select all endpoint names and IPv4
      $storageAccountReport | Select StorageAccountName,SubscriptionName,ResourceGroupName,PrimaryBlobDNS,PrimaryBlobIP4Address,PrimaryQueueDNS,PrimaryQueueIP4Address,PrimaryTableDNS,PrimaryTableIP4Address,PrimaryFileDNS,PrimaryFileIP4Address,PrimaryWebDNS,PrimaryWebIP4Address,PrimaryDfsDNS,PrimaryDfsIP4Address,PrimaryMicrosoftEndpointsDNS,PrimaryMicrosoftEndpointsIP4Address,PrimaryInternetEndpointsDNS,PrimaryInternetEndpointsIP4Address,SecondaryBlobDNS,SecondaryBlobIP4Address,SecondaryQueueDNS,SecondaryQueueIP4Address,SecondaryTableDNS,SecondaryTableIP4Address,SecondaryFileDNS,SecondaryFileIP4Address,SecondaryWebDNS,SecondaryWebIP4Address,SecondaryDfsDNS,SecondaryDfsIP4Address,SecondaryMicrosoftEndpointsDNS,SecondaryMicrosoftEndpointsIP4Address,SecondaryInternetEndpointsDNS,SecondaryInternetEndpointsIP4Address
 
      Export to CSV
      $storageAccountReport | Select StorageAccountName,SubscriptionName,ResourceGroupName,PrimaryBlobDNS,PrimaryBlobIP4Address,PrimaryQueueDNS,PrimaryQueueIP4Address,PrimaryTableDNS,PrimaryTableIP4Address,PrimaryFileDNS,PrimaryFileIP4Address,PrimaryWebDNS,PrimaryWebIP4Address,PrimaryDfsDNS,PrimaryDfsIP4Address,PrimaryMicrosoftEndpointsDNS,PrimaryMicrosoftEndpointsIP4Address,PrimaryInternetEndpointsDNS,PrimaryInternetEndpointsIP4Address,SecondaryBlobDNS,SecondaryBlobIP4Address,SecondaryQueueDNS,SecondaryQueueIP4Address,SecondaryTableDNS,SecondaryTableIP4Address,SecondaryFileDNS,SecondaryFileIP4Address,SecondaryWebDNS,SecondaryWebIP4Address,SecondaryDfsDNS,SecondaryDfsIP4Address,SecondaryMicrosoftEndpointsDNS,SecondaryMicrosoftEndpointsIP4Address,SecondaryInternetEndpointsDNS,SecondaryInternetEndpointsIP4Address | Export-Csv storageaccountreport.csv -NoTypeInformation
-
 
 #>
 
@@ -62,7 +65,10 @@ ForEach ($subscription in $subscriptions){
     $storageAccounts = Get-AzStorageAccount
 
     forEach ($storageAccount in $storageAccounts){
-    
+
+        $primaryIP4Addresses = @()
+        $secondaryIP4Addresses = @()
+
         ForEach ($endpoint in $($storageAccount.PrimaryEndpoints | ConvertTo-Xml).Objects.Object.Property){
 
             if ($endpoint.'#text'){
@@ -74,6 +80,7 @@ ForEach ($subscription in $subscriptions){
             $storageAccount | Add-Member -MemberType NoteProperty -Name ('Primary{0}DNS' -f $endpoint.Name) -Value $dnsObject.Name[0] -Force
             $storageAccount | Add-Member -MemberType NoteProperty -Name ('Primary{0}IP4Address' -f $endpoint.Name) -Value $dnsObject.IP4Address -Force
             $storageAccount | Add-Member -MemberType NoteProperty -Name 'SubscriptionName' -Value $subscription.Name -Force
+            $primaryIP4Addresses += $dnsObject.IP4Address
         }
         
         
@@ -88,7 +95,11 @@ ForEach ($subscription in $subscriptions){
             $storageAccount | Add-Member -MemberType NoteProperty -Name ('Secondary{0}DNS' -f $endpoint.Name) -Value $dnsObject.Name[0] -Force
             $storageAccount | Add-Member -MemberType NoteProperty -Name ('Secondary{0}IP4Address' -f $endpoint.Name) -Value $dnsObject.IP4Address -Force
             $storageAccount | Add-Member -MemberType NoteProperty -Name 'SubscriptionName' -Value $subscription.Name -Force
+            $secondaryIP4Addresses += $dnsObject.IP4Address
         }
+
+        $storageAccount | Add-Member -MemberType NoteProperty -Name 'PrimaryIP4Addresses' -Value $primaryIP4Addresses -Force
+        $storageAccount | Add-Member -MemberType NoteProperty -Name 'SecondaryIP4Addresses' -Value $secondaryIP4Addresses -Force
     }
 
     $allStorageAccounts += $storageAccounts
