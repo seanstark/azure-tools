ac1b6ccf7c3534e71d25651e1733bd967ef43d76
seanstark-ms <84108246+seanstark@users.noreply.github.com>
2022-04-26 11:06:20 -0700

    updated directories

---
+++ defender-for-cloud/examples.ps1
@@ -0,0 +1,28 @@
+﻿$put = .\new-fimitem.ps1 -SubscriptionId 'c94dffc7-2659-4750-a34e-a160f2a68c90' `
+-ResourceGroup 'sentinel-prd' `
+-Workspace 'sentinel-prd' `
+-Kind WindowsRegistry `
+-ItemName 'Test3' `
+-RegistryKey 'HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Explorer\testno-rescu' `
+-Recursive
+
+
+$put = .\new-fimitem.ps1 -SubscriptionId 'c94dffc7-2659-4750-a34e-a160f2a68c90' `
+-ResourceGroup 'sentinel-prd' `
+-Workspace 'sentinel-prd' `
+-Kind LinuxFiles `
+-ItemName 'Test_Linux' `
+-Path '/etc/txt.txt' `
+-PathType File `
+-Links Ignore `
+-UseSudo $false
+
+
+
+$put = .\new-fimitem.ps1 -SubscriptionId 'c94dffc7-2659-4750-a34e-a160f2a68c90' `
+-ResourceGroup 'sentinel-prd' `
+-Workspace 'sentinel-prd' `
+-Kind WindowsFiles `
+-ItemName 'Test_Windows' `
+-Path 'C:\windows\' `
+-PathType Directory 
